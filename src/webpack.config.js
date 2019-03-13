2b8f8844297c186f944f5c7d15955e43f0615e0c
Espen Norderud <espen42@gmail.com>
2019-03-13 21:45:44 +0100

    Outputs stats.json, for specific tracking of each entry's dependency. Added test, including generation of react4xp build constants.

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -13,14 +13,23 @@
 // XP component (part, page, etc) but can still use the second-level dependency chunks mentioned above.
 const Chunks2json = require('chunks-2-json-webpack-plugin');
 const React4xpEntriesAndChunks = require('react4xp-build-entriesandchunks');
+const StatsPlugin = require('stats-webpack-plugin');
+const path = require('path');
 
 module.exports = env => {
+    env = env || {};
+
     const {
         SRC_R4X, R4X_ENTRY_SUBFOLDER, BUILD_R4X, BUILD_ENV, LIBRARY_NAME, EXTERNALS,
         CHUNK_CONTENTHASH, COMPONENT_CHUNKS_FILENAME, ENTRIES_FILENAME,
         recommended
     } = require(env.REACT4XP_CONFIG_FILE);
 
+    console.log({
+        SRC_R4X, R4X_ENTRY_SUBFOLDER, BUILD_R4X, BUILD_ENV, LIBRARY_NAME, EXTERNALS,
+        CHUNK_CONTENTHASH, COMPONENT_CHUNKS_FILENAME, ENTRIES_FILENAME,
+        recommended
+    }, null, 2);//*/
 
     const DEVMODE = (BUILD_ENV !== 'production');
 
@@ -76,6 +85,7 @@ module.exports = env => {
                     loader: 'babel-loader',
                     query: {
                         compact: !DEVMODE,
+                        presets: ["react"]
                     },
                 }, {
                     test: /\.less$/,
@@ -95,6 +105,90 @@ module.exports = env => {
 
         plugins: [
             new Chunks2json({outputDir: BUILD_R4X, filename: COMPONENT_CHUNKS_FILENAME}),
-        ]
+            new StatsPlugin('stats.json',
+                {
+                    // Add asset Information
+                    assets: false,
+
+                    // Add build date and time information
+                    builtAt: false,
+
+                    // Add information about cached (not built) modules
+                    cached: false,
+
+                    // Show cached assets (setting this to `false` only shows emitted files)
+                    cachedAssets: false,
+
+                    // Add children information
+                    children: false,
+
+                    // Add chunk information (setting this to `false` allows for a less verbose output)
+                    chunks: false,
+
+                    // Add namedChunkGroups information
+                    chunkGroups: false,
+
+                    // Add built modules information to chunk information
+                    chunkModules: false,
+
+                    // Add the origins of chunks and chunk merging info
+                    chunkOrigins: false,
+
+                    // Context directory for request shortening
+                    //context: "../src/",
+
+                    // Display the distance from the entry point for each module
+                    depth: false,
+
+                    // Display the entry points with the corresponding bundles
+                    entrypoints: true,                                          // <-- THE IMPORTANT ONE, FOR DEPENDENCY TRACKING!
+
+                    // Add --env information
+                    env: false,
+
+                    // Add errors
+                    errors: true,
+
+                    // Add details to errors (like resolving log)
+                    errorDetails: false,
+
+                    // Add the hash of the compilation
+                    hash: false,
+
+                    // Add built modules information
+                    modules: false,
+
+                    // Show dependencies and origin of warnings/errors (since webpack 2.5.0)
+                    moduleTrace: false,
+
+                    // Show performance hint when file size exceeds `performance.maxAssetSize`
+                    performance: false,
+
+                    // Show the exports of the modules
+                    providedExports: false,
+
+                    // Add public path information
+                    publicPath: false,
+
+                    // Add information about the reasons why modules are included
+                    reasons: false,
+
+                    // Add the source code of modules
+                    source: false,
+
+                    // Add timing information
+                    timings: false,
+
+                    // Show which exports of a module are used
+                    usedExports: false,
+
+                    // Add webpack version information
+                    version: false,
+
+                    // Add warnings
+                    warnings: true,
+                }
+            )
+        ],
     };
 };
---
+++ test/src/_components/Greeter.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import WorldGreeter from '../sharedComps/ReduxWorldGreeter';
+
+export default (props) => <WorldGreeter {...props} />;
