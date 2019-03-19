d2998ce164ed356aad48a3e94d0170528d7cab0f
Espen Norderud <espen42@gmail.com>
2019-03-19 23:49:37 +0100

    Bad, possibly. Updating for new standalone rendering - renderWithDependencies. But something about the constants that should determine the service url is not working. Which lib or package is the culprit?

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -11,23 +11,21 @@
 //
 // - ...and in the XP structure under src/main/resources/site: Here, JSX files are more tightly bound to their corresponding
 // XP component (part, page, etc) but can still use the second-level dependency chunks mentioned above.
-const Chunks2json = require('chunks-2-json-webpack-plugin');
 const React4xpEntriesAndChunks = require('react4xp-build-entriesandchunks');
 const StatsPlugin = require('stats-webpack-plugin');
-const path = require('path');
 
 module.exports = env => {
     env = env || {};
 
     const {
         SRC_R4X, R4X_ENTRY_SUBFOLDER, BUILD_R4X, BUILD_ENV, LIBRARY_NAME, EXTERNALS,
-        CHUNK_CONTENTHASH, COMPONENT_CHUNKS_FILENAME, ENTRIES_FILENAME,
+        COMPONENT_STATS_FILENAME, CHUNK_CONTENTHASH, ENTRIES_FILENAME,
         recommended
     } = require(env.REACT4XP_CONFIG_FILE);
 
     console.log({
         SRC_R4X, R4X_ENTRY_SUBFOLDER, BUILD_R4X, BUILD_ENV, LIBRARY_NAME, EXTERNALS,
-        CHUNK_CONTENTHASH, COMPONENT_CHUNKS_FILENAME, ENTRIES_FILENAME,
+        COMPONENT_STATS_FILENAME, CHUNK_CONTENTHASH, ENTRIES_FILENAME,
         recommended
     }, null, 2);//*/
 
@@ -104,89 +102,37 @@ module.exports = env => {
         },
 
         plugins: [
-            new Chunks2json({outputDir: BUILD_R4X, filename: COMPONENT_CHUNKS_FILENAME}),
-            new StatsPlugin('stats.json',
+            new StatsPlugin(COMPONENT_STATS_FILENAME,
                 {
-                    // Add asset Information
-                    assets: false,
-
-                    // Add build date and time information
-                    builtAt: false,
-
-                    // Add information about cached (not built) modules
-                    cached: false,
-
-                    // Show cached assets (setting this to `false` only shows emitted files)
-                    cachedAssets: false,
-
-                    // Add children information
-                    children: false,
-
-                    // Add chunk information (setting this to `false` allows for a less verbose output)
-                    chunks: false,
-
-                    // Add namedChunkGroups information
-                    chunkGroups: false,
-
-                    // Add built modules information to chunk information
-                    chunkModules: false,
-
-                    // Add the origins of chunks and chunk merging info
-                    chunkOrigins: false,
-
-                    // Context directory for request shortening
-                    //context: "../src/",
-
-                    // Display the distance from the entry point for each module
-                    depth: false,
-
                     // Display the entry points with the corresponding bundles
                     entrypoints: true,                                          // <-- THE IMPORTANT ONE, FOR DEPENDENCY TRACKING!
-
-                    // Add --env information
-                    env: false,
-
-                    // Add errors
                     errors: true,
-
-                    // Add details to errors (like resolving log)
-                    errorDetails: false,
-
-                    // Add the hash of the compilation
-                    hash: false,
-
-                    // Add built modules information
-                    modules: false,
-
-                    // Show dependencies and origin of warnings/errors (since webpack 2.5.0)
-                    moduleTrace: false,
-
-                    // Show performance hint when file size exceeds `performance.maxAssetSize`
-                    performance: false,
-
-                    // Show the exports of the modules
-                    providedExports: false,
-
-                    // Add public path information
-                    publicPath: false,
-
-                    // Add information about the reasons why modules are included
-                    reasons: false,
-
-                    // Add the source code of modules
-                    source: false,
-
-                    // Add timing information
-                    timings: false,
-
-                    // Show which exports of a module are used
-                    usedExports: false,
-
-                    // Add webpack version information
-                    version: false,
-
-                    // Add warnings
                     warnings: true,
+
+                    // Everything else switched off:
+                    assets: false,
+                    builtAt: false,
+                    cached: false,
+                    cachedAssets: false,
+                    children: false,
+                    chunks: false,
+                    chunkGroups: false,
+                    chunkModules: false,
+                    chunkOrigins: false,
+                    depth: false,
+                    env: false,
+                    errorDetails: false,
+                    hash: false,
+                    modules: false,
+                    moduleTrace: false,
+                    performance: false,
+                    providedExports: false,
+                    publicPath: false,
+                    reasons: false,
+                    source: false,
+                    timings: false,
+                    usedExports: false,
+                    version: false,
                 }
             )
         ],
