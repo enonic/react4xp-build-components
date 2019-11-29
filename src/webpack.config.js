d91368ae0acf6f5538db6aa804589e4abf91e2cd
Espen Norderud <espen42@gmail.com>
2019-11-29 18:11:13 +0100

    ChunkDirs and EntryDirs are parsed better. Still needs more testing.

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -17,6 +17,108 @@ const StatsPlugin = require("stats-webpack-plugin");
 const path = require("path");
 const fs = require("fs");
 
+// Turns a comma-separated list of subdirectories below the root React4xp source folder (SRC_R4X) into an array of unique,
+// verified, absolute-path'd and OS-compliant folder names. Halts on errors, displays warnings, skips items that are not found.
+const normalizeDirList = (
+  commaSepDirList,
+  singularLabel,
+  SRC_R4X,
+  symlinksUnderReact4xpRoot,
+  VERBOSE
+) =>
+  (commaSepDirList || "").trim()
+    ? Array.from(
+        new Set(
+          commaSepDirList
+            .trim()
+            .replace(/[\\/]/g, path.sep)
+            .replace(/[´`'"]/g, "")
+            .split(",")
+
+            .map(item => (item || "").trim())
+            .filter(item => !!item)
+            .map(item => item.replace(/[\\/]$/, ""))
+            .map(orig => {
+              let dir = path.resolve(path.join(SRC_R4X, orig));
+
+              let realDir = null;
+              try {
+                realDir = fs.realpathSync(dir);
+              } catch (e) {
+                if (VERBOSE) {
+                  console.warn(
+                    `Warning - error message dump for ${singularLabel} '${orig}':\n--------`
+                  );
+                  console.warn(e);
+                }
+                console.warn(
+                  `${
+                    VERBOSE ? "-------->" : "Warning:"
+                  } skipping ${singularLabel} '${orig}' from react4xp.properties${
+                    !VERBOSE
+                      ? " - it probably just doesn't exist. If you're sure it exists, there may be another problem - run the build again with verbose option in react4xp.properties for full error dump"
+                      : ""
+                  }.`
+                );
+                return null;
+              }
+
+              let symlinkTargetDir = null;
+              let lstat = fs.lstatSync(dir);
+              while (lstat.isSymbolicLink()) {
+                symlinkTargetDir = fs.readlinkSync(dir);
+                dir = path.resolve(dir, "..", symlinkTargetDir);
+
+                if (fs.existsSync(dir)) {
+                  if (dir.startsWith(SRC_R4X)) {
+                    // eslint-disable-next-line no-param-reassign
+                    symlinksUnderReact4xpRoot[orig] = true;
+                  }
+                  lstat = fs.lstatSync(dir);
+                } else {
+                  throw Error(
+                    `${singularLabel.replace(/^\w/, c =>
+                      c.toUpperCase()
+                    )} '${orig}' from react4xp.properties leads by resolved symlink(s) to '${dir}', which was not found.`
+                  );
+                }
+              }
+
+              lstat = fs.lstatSync(realDir);
+              if (!lstat.isDirectory()) {
+                throw Error(
+                  `Can't add ${singularLabel} '${orig}' from react4xp.properties - ${realDir} was found but is not a directory.`
+                );
+              }
+
+              return realDir;
+            })
+            .filter(dir => !!dir)
+        )
+      )
+    : [];
+
+const makeExclusionsString = (currentDir, otherDirs) =>
+  otherDirs
+    .filter(dir => dir !== currentDir && dir.startsWith(currentDir))
+    .map(dir => dir.slice(currentDir.length))
+    .map(d => {
+      let dir = d;
+      if (dir.startsWith(path.sep)) {
+        dir = dir.slice(1);
+      }
+      if (dir.endsWith(path.sep)) {
+        dir = dir.slice(0, dir.length - 1);
+      }
+      console.log("\t==>", dir, "\n");
+      return dir;
+    })
+    // TODO: escape characters in folder names that actually are regexp characters
+    .join("|")
+    .trim();
+
+// -------------------------------------------------------------
+
 module.exports = (env = {}) => {
   const {
     SRC_R4X,
@@ -58,49 +160,48 @@ module.exports = (env = {}) => {
     }
   }
 
-  const chunkDirs = env.CHUNK_DIRS
-    ? env.CHUNK_DIRS.trim()
-        .replace(/[´`'"]/g, "")
-        .split(",")
-        .map(dir => dir.replace(/[\\/]/g, path.sep))
-        .map(dir => path.resolve(path.join(SRC_R4X, dir.trim())))
-        .filter(dir => {
-          if (fs.existsSync(dir)) {
-            if (!fs.lstatSync(dir).isDirectory()) {
-              throw Error(
-                `Can't add chunkDir: ${dir} was found but is not a directory. Probably check chunkDirs in react4xp.properties.`
-              );
-            }
-            return true;
-          }
-          console.warn(
-            `ChunkDir from react4xp.properties was not found. Skipping: ${dir}`
-          );
-          return false;
-        })
-    : [];
+  // -------------------------------------  Okay, settings and context are parsed. Let's go:
 
-  const entryDirs = env.ENTRY_DIRS
-    ? env.ENTRY_DIRS.trim()
-        .replace(/[´`'"]/g, "")
-        .split(",")
-        .map(dir => dir.replace(/[\\/]/g, path.sep))
-        .map(dir => path.resolve(path.join(SRC_R4X, dir.trim())))
-        .filter(dir => {
-          if (fs.existsSync(dir)) {
-            if (!fs.lstatSync(dir).isDirectory()) {
-              throw Error(
-                `Can't add entryDir: ${dir} was found but is not a directory. Probably check entryDirs in react4xp.properties.`
-              );
-            }
-            return true;
-          }
-          console.warn(
-            `EntryDir from react4xp.properties was not found. Skipping: ${dir}`
-          );
-          return false;
-        })
-    : [];
+  let symlinksUnderReact4xpRoot = {};
+
+  const chunkDirs = normalizeDirList(
+    env.CHUNK_DIRS,
+    "chunkDir",
+    SRC_R4X,
+    symlinksUnderReact4xpRoot,
+    VERBOSE
+  );
+  const entryDirs = normalizeDirList(
+    env.ENTRY_DIRS,
+    "entryDir",
+    SRC_R4X,
+    symlinksUnderReact4xpRoot,
+    VERBOSE
+  );
+
+  // Catching some likely troublemakers:
+  symlinksUnderReact4xpRoot = Object.keys(symlinksUnderReact4xpRoot).filter(
+    key => !!symlinksUnderReact4xpRoot[key]
+  );
+  if (symlinksUnderReact4xpRoot.length) {
+    console.warn(
+      `Warning: ${
+        symlinksUnderReact4xpRoot.length
+      } chunkDir(s) / entryDir(s) in react4xp.properties are symlinks that lead inside the folder structure below the React4xp root (${SRC_R4X}). This could cause a mess in React4xp's entry/chunk structure, so I hope you know what you're doing. Culprits: ${symlinksUnderReact4xpRoot.join(
+        ", "
+      )}`
+    );
+  }
+  const duplicates = chunkDirs.filter(dir => entryDirs.indexOf(dir) !== -1);
+  if (duplicates.length) {
+    throw Error(
+      `${
+        duplicates.length
+      } directories are listed both as chunkDirs and entryDirs in react4xp.properties: ${duplicates.join(
+        ", "
+      )}`
+    );
+  }
 
   const entryExtensions = (env.ENTRY_EXT || "")
     .trim()
@@ -109,17 +210,6 @@ module.exports = (env = {}) => {
     .map(ext => ext.trim())
     .map(ext => ext.replace(/^\./, ""))
     .filter(ext => !!ext);
-
-  console.log(
-    `chunkDirs (${
-      Array.isArray(chunkDirs)
-        ? `array[${chunkDirs.length}]`
-        : typeof chunkDirs +
-          (chunkDirs && typeof chunkDirs === "object"
-            ? ` with keys: ${JSON.stringify(Object.keys(chunkDirs))}`
-            : "")
-    }): ${JSON.stringify(chunkDirs, null, 2)}`
-  );
 
   const entrySets = [
     {
@@ -162,23 +252,8 @@ module.exports = (env = {}) => {
     );
   }
 
-  const react4xpExclusions = [...entryDirs, ...chunkDirs]
-    .filter(dir => dir.startsWith(SRC_R4X))
-    .map(dir => dir.slice(SRC_R4X.length))
-    .map(d => {
-      let dir = d;
-      if (dir.startsWith(path.sep)) {
-        dir = dir.slice(1);
-      }
-      if (dir.endsWith(path.sep)) {
-        dir = dir.slice(0, dir.length - 1);
-      }
-      console.log("\t==>", dir, "\n");
-      return dir;
-    })
-    // TODO: escape characters in folder names that actually are regexp characters
-    .join("|")
-    .trim();
+  const detectedTargetDirs = [...entryDirs, ...chunkDirs];
+  const react4xpExclusions = makeExclusionsString(SRC_R4X, detectedTargetDirs);
 
   const cacheGroups = {
     vendors: {
@@ -200,12 +275,9 @@ module.exports = (env = {}) => {
       enforce: true,
       chunks: "all",
       priority: 1,
-      test: /* new RegExp( */ `${SRC_R4X}${
-        // TODO: re-regexp this!
+      test: `${SRC_R4X}${
         react4xpExclusions ? `[\\/]((?!(${react4xpExclusions})).)[\\/]` : ""
       }`
-      /* , 'g')
-       */
     }
   };
 
@@ -223,27 +295,10 @@ module.exports = (env = {}) => {
     }
     takenNames.push(name);
 
-    const chunkExclusions = [...entryDirs, ...chunkDirs]
-      .filter(dir => dir !== chunkDir && dir.startsWith(chunkDir))
-      .map(dir => dir.slice(chunkDir.length))
-      .map(d => {
-        let dir = d;
-        if (dir.startsWith(path.sep)) {
-          dir = dir.slice(1);
-        }
-        if (dir.endsWith(path.sep)) {
-          dir = dir.slice(0, dir.length - 1);
-        }
-        console.log("\t==>", dir, "\n");
-        return dir;
-      })
-      // TODO: escape characters in folder names that actually are regexp characters
-      .join("|")
-      .trim();
-    const test = /* new RegExp( */ `${chunkDir}${
-      // TODO: re-regexp this!
+    const chunkExclusions = makeExclusionsString(chunkDir, detectedTargetDirs);
+    const test = `${chunkDir}${
       chunkExclusions ? `[\\/]((?!(${chunkExclusions})).)[\\/]` : ""
-    }`; /* , 'g') */
+    }`;
 
     cacheGroups[name] = {
       name,
@@ -254,22 +309,30 @@ module.exports = (env = {}) => {
     };
   });
 
+  // Display the generated cachegroups:
   if (VERBOSE) {
     console.log(
-      `cacheGroups (${
-        Array.isArray(cacheGroups)
-          ? `array[${cacheGroups.length}]`
-          : typeof cacheGroups +
-            (cacheGroups && typeof cacheGroups === "object"
-              ? ` with keys: ${JSON.stringify(Object.keys(cacheGroups))}`
-              : "")
-      }): ${JSON.stringify(cacheGroups, null, 2)}`
+      `Generated cacheGroups (.test attributes that are strings will be turned into RegExp): ${JSON.stringify(
+        cacheGroups,
+        null,
+        2
+      )}`
     );
   }
 
+  // Finally, turn all generated regexp strings in each .test attribute into actual RegExp's:
+  Object.keys(cacheGroups).forEach(key => {
+    cacheGroups[key] = {
+      ...cacheGroups[key],
+      test:
+        typeof cacheGroups[key].test === "string"
+          ? new RegExp(cacheGroups[key].test)
+          : cacheGroups[key].test
+    };
+  });
+
   // Decides whether or not to hash filenames of common-component chunk files, and the length of the hash
   let chunkFileName;
-
   if (!CHUNK_CONTENTHASH) {
     chunkFileName = "[name].js";
   } else if (typeof CHUNK_CONTENTHASH === "string") {
